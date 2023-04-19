a9e488f3f64ef7433c0c5bb8570a1e5508d9d909
grafvetkindorff <vetkindorff@gmail.com>
2023-04-19 09:34:23 +0300

    [clear-repl] temporarly changed output file location

--- clear_repl.cljs
+++ clear_repl.cljs
@@ -1,10 +1,12 @@
 (ns clear-repl
   (:require ["vscode" :as vscode]
+            ["fs" :as fs]
+            ["path" :as path]
             ["ext://betterthantomorrow.calva$v0" :as calva]
             [joyride.core :as joyride]
             [promesa.core :as p]
             [clojure.string :as str]
-            [util.editor :as editor-utils]))
+            #_[util.editor :as editor-utils]))
 
 
 (def root-path (-> (first vscode/workspace.workspaceFolders) .-uri .-fsPath))
@@ -14,4 +16,13 @@
   (fs/writeFileSync (path/resolve root-path path) content))
 
 
-(write-to-file ".calva/output-window/output.calva-repl" "")
+;; TODO: get current document and if it is a .calva-repl file, clear it
+#_(defn current-document []
+  (let [editor ^js vscode/window.activeTextEditor
+        document (.-document editor)]
+    document))
+
+#_(vscode/window.showInformationMessage (.document (editor-utils/current-document)))
+
+(write-to-file "box/.calva/output-window/output.calva-repl" "")
+
